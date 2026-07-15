7f7e41b86ae11175cb46f0e979426b10cee8cd57
Zuiun1kya9 <Zuiun1kya9@users.noreply.github.com>
2026-07-15 21:19:28 +0900

    Randomize answer choices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vba-sprint-v1';
+const CACHE='vba-sprint-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
